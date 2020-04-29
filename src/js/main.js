e5533651818d81d4078743a70d77bc5a5097cfb7
v-lavs <lavskavanda@gmail.com>
2020-04-29 15:28:18 +0300

    section-banner sketch

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+/**
+ *To include js file from libraries write: `//= include ./path-to-file`
+ * */
+
+//= include ../lib/jquery-3.3.1.min.js
+
+
+/**
+ * CUSTOM SCRIPTS
+ **/
+
+$(document).ready(function () {
+
+
+});
