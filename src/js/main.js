5f90a4a9b47e5838d3b1b59f29d2d18aa832b2ef
v-lavs <lavskavanda@gmail.com>
2020-05-01 22:39:58 +0300

    111

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,7 @@
  * */
 
 //= include ../lib/jquery-3.3.1.min.js
-//= include ../lib/jquery-nice-select-1.1.0/js/jquery.js
 //= include ../lib/jquery-nice-select-1.1.0/js/jquery.nice-select.js
-
 
 
 /**
@@ -13,33 +11,78 @@
  **/
 
 $(document).ready(function () {
-    var swiperBrands = new Swiper('.slider-brands', {
-        slidesPerView: 3,
-        spaceBetween: 90,
-        navigation: {
-            nextEl: '.block-view-brands .swiper-button-next',
-            prevEl: '.block-view-brands .swiper-button-prev',
-        },
-        pagination: {
-            el: '.block-view-brands .swiper-pagination',
-        },
-    });
+    if ($('.slider-brands .card').length > 3) {
+
+        $('#popularBrandsPagination').addClass('brands-pagination_active');
+        $('.slider-brands').removeClass('slider-brands_static');
+
+        var swiperBrands = new Swiper('.slider-brands', {
+            navigation: {
+                nextEl: '#popularBrandsPagination .swiper-button-next',
+                prevEl: '#popularBrandsPagination .swiper-button-prev',
+            },
+            pagination: {
+                el: '#popularBrandsPagination .swiper-pagination',
+            },
+            breakpoints: {
+                640: {
+                    slidesPerView: 1,
+                },
+                1050: {
+                    slidesPerView: 2,
+                },
+                1150: {
+                    slidesPerView: 3,
+                }
+            }
+        });
+    }
+
 
     var swiperReviews = new Swiper('.slider-reviews', {
         direction: 'vertical',
         navigation: {
-            nextEl: '.block-reviews  .swiper-button-next',
-            prevEl: '.block-reviews  .swiper-button-prev',
+            nextEl: '#reviewsPagination  .swiper-button-next',
+            prevEl: '#reviewsPagination  .swiper-button-prev',
         },
         pagination: {
-            el: '.block-reviews .swiper-pagination',
+            el: '#reviewsPagination .swiper-pagination',
             clickable: true,
         },
     });
+
     /**
      * CUSTOM SELECT
      **/
-    $(document).ready(function() {
-        $('select').niceSelect();
-    });
+
+    $('.custom-select').niceSelect();
+
+    /**
+     * CARD HEIGHT
+     **/
+
+    function setEqualHeight() {
+
+        var max_card_height = 0;
+        var $advantagesCards = $('.advantages-card');
+        $advantagesCards.each(function () {
+            if ($(this).height() > max_card_height) { // если высота колонки больше значения максимальной высоты,
+                max_card_height = $(this).height(); // то она сама становится новой максимальной высотой
+            }
+        });
+        $advantagesCards.css({minHeight: max_card_height}); // устанавливаем высоту каждой колонки равной значению максимальной высоты
+    }
+
+    setEqualHeight();
+
+    var windowW = $(window).width();
+
+    $(window).on('resize', function () {
+        var resizedWindowWidth = $(window).width();
+
+        if (windowW !== resizedWindowWidth) {
+            windowW = resizedWindowWidth;
+            setEqualHeight();
+        }
+    })
 });
